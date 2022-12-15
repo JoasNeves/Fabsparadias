f5004c75515280d0c0b23c0639fc855d7d7bdfe4
JoasNeves <120684465+JoasNeves@users.noreply.github.com>
2022-12-15 18:47:17 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var x = 0;
+
+
+function msg() {
+    x+=1;
+    alert("Você clicou no botão! Clicaram "+x+" Vezes no botão.");
+}
+
+function comprou(){
+    alert("Erro:Erro:Erro:Erro:Erro:Erro:Erro:Erro:Erro:Erro:Erro:Erro:Erro:Erro:Erro:Erro:Erro:Erro:Erro:Erro:Erro");
+}
